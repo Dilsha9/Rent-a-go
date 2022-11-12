8107ee6bb3d7120ed874542bacf1c4d6941cd0a5
Dilsha Shirin <dilshashirin81@gmail.com>
2022-11-12 22:40:26 +0530

    added product detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { ShoppingComponent } from './shopping/shopping.component';
+import { DescriptionComponent } from './description/description.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    ShoppingComponent
+    ShoppingComponent,
+    DescriptionComponent,
+    
   ],
   imports: [
     BrowserModule,
